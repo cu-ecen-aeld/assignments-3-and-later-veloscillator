f0fa0edba3411ea8d4e6919c4fd9e17a6b643886
Ken Kochis <ken@pex.com>
2023-03-13 18:32:48 -0700

    Assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,11 @@
 #include "systemcalls.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -11,13 +18,11 @@ bool do_system(const char *cmd)
 {
 
 /*
- * TODO  add your code here
  *  Call the system() function with the command set in the cmd
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    return !system(cmd);
 }
 
 /**
@@ -45,12 +50,9 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+    va_end(args);
 
 /*
- * TODO:
  *   Execute a system command by calling fork, execv(),
  *   and wait instead of system (see LSP page 161).
  *   Use the command[0] as the full path to the command to execute
@@ -58,10 +60,20 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
-
-    va_end(args);
-
-    return true;
+    int pid;
+    if ((pid = fork()) == 0) {
+        // In child. exec.
+        if (execv(command[0], command) == -1)
+            exit(errno);
+    } else if (pid == -1) {
+        // Error forking.
+        return false;
+    }
+    // In parent, wait for child to complete.
+    int status;
+    if (waitpid(pid, &status, 0) == -1)
+        return false;
+    return status == 0;
 }
 
 /**
@@ -83,17 +95,36 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
     command[count] = command[count];
-
+    va_end(args);
 
 /*
- * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
  *   redirect standard out to a file specified by outputfile.
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    // Open output file.
+    int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd == -1)
+        return false;
 
-    va_end(args);
-
-    return true;
+    int pid;
+    if ((pid = fork()) == 0) {
+        // In child. Redirect stdout (1) to outputfile. exec.
+        if (dup2(fd, 1) == -1)
+            exit(errno);
+        close(fd);
+        if (execv(command[0], command) == -1)
+            exit(errno);
+    }
+    close(fd);
+    if (pid == -1) {
+        // Error forking.
+        return false;
+    }
+    // In parent, wait for child to complete.
+    int status;
+    if (waitpid(pid, &status, 0) == -1)
+        return false;
+    return status == 0;
 }
