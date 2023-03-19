2def3b8a0af2b5fd4631399a5f1447d3953b49cf
Ken Kochis <ken@pex.com>
2023-03-18 22:59:25 -0700

    Assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,8 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+MAKEFLAGS=-j$(nproc)
+SYSROOT=`${CROSS_COMPILE}gcc -print-sysroot`
 
 if [ $# -lt 1 ]
 then
@@ -25,7 +27,7 @@ mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
+    # Clone only if the repository does not exist.
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
@@ -34,7 +36,11 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper  # Deep clean, remove config.
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig # Setup default config.
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all       # Build vmlinux.
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules   # Build modules.
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs      # Build devicetree.
 fi
 
 echo "Adding the Image in outdir"
@@ -44,10 +50,15 @@ cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+    sudo rm -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+ROOT="${OUTDIR}/rootfs"
+mkdir -p "$ROOT"
+mkdir "${ROOT}/bin" "${ROOT}/dev" "${ROOT}/etc" "${ROOT}/home" "${ROOT}/lib" "${ROOT}/lib64" "${ROOT}/proc" \
+      "${ROOT}/sbin" "${ROOT}/sys" "${ROOT}/tmp" "${ROOT}/usr" "${ROOT}/usr/bin" "${ROOT}/usr/sbin" \
+      "${ROOT}/var" "${ROOT}/var/log"
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +66,54 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${ROOT} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+regex='/[a-zA-Z0-9\./-]*'
+interpreter=`${CROSS_COMPILE}readelf -a "${ROOT}/bin/busybox" | grep "program interpreter" | grep -o '/[a-zA-Z0-9\./-]*'`
+libs=`${CROSS_COMPILE}readelf -a "${ROOT}/bin/busybox" | grep "Shared library" | grep -o '\[.*\]'`
 
-# TODO: Add library dependencies to rootfs
+# Add program interpreter and library dependencies to rootfs.
+cp "${SYSROOT}${interpreter}" "${ROOT}/lib/"
+for lib in $libs; do
+    cp "${SYSROOT}/lib64/${lib:1:-1}" "${ROOT}/lib64/"
+done
 
-# TODO: Make device nodes
+# Make device nodes
+sudo mknod "${ROOT}/dev/null" -m 666 c 1 3
+sudo mknod "${ROOT}/dev/console" -m 666 c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+cd "${FINDER_APP_DIR}"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp writer "${ROOT}/home/"
+cp writer.sh "${ROOT}/home/"
+cp finder.sh "${ROOT}/home/"
+cp -r ../conf "${ROOT}/home/"
+cp finder-test.sh "${ROOT}/home/"
+cp autorun-qemu.sh "${ROOT}/home/"
 
-# TODO: Chown the root directory
+# Chown the root directory
+sudo chown -R root "${ROOT}"
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+cd "${ROOT}"
+rm -f "${OUTDIR}/initramfs.cpio.gz"
+find . | cpio -H newc -ov --owner root:root > "${OUTDIR}/initramfs.cpio"
+gzip "${OUTDIR}/initramfs.cpio"
+cd "${FINDER_APP_DIR}"
+
+# Copy kernel Image.
+cp "${OUTDIR}/linux-stable/arch/arm64/boot/Image" "${OUTDIR}/"
